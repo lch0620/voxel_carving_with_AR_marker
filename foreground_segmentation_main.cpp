16180e9640e0f3e6d57a0a050efbbd0652fe4fb4
ling_hsuan_hsu <sharon201306@gmail.com>
2024-06-28 13:12:52 +0200

    modified segmentation_main to save the output

--- foreground_segmentation_main.cpp
+++ foreground_segmentation_main.cpp
@@ -2,12 +2,13 @@
 
 int main(int argc, char** argv) {
     string imageFolder = "C:/Users/user/OneDrive/桌面/3D scanning/Exercises/voxel_carving_with_AR_marker/Data/Ferrari";
+    string outputFolder = "C:/Users/user/OneDrive/桌面/3D scanning/Exercises/voxel_carving_with_AR_marker/Data/SegmentedImages_original_size";
     Scalar COLOR_MIN(15, 45, 45);
     Scalar COLOR_MAX(65, 255, 255);
     int targetWidth = 1200;
     int targetHeight = 1000;
 
-    ImageSegmentation segmenter(imageFolder, COLOR_MIN, COLOR_MAX, targetWidth, targetHeight);
+    ImageSegmentation segmenter(imageFolder, , outputFolder, COLOR_MIN, COLOR_MAX, targetWidth, targetHeight);
     segmenter.performSegmentation();
 
     return 0;
